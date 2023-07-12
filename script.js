f0aac5a93eb00e3996cf7afe8e35a1a39e2538a7
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-07-12 17:59:41 -0300

    header funcionalities

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const menuIcon = document.getElementById("i");
 const closeMenu = document.getElementById("ii");
 const navMobile = document.getElementById("nav");
-const header = document.querySelector("header");
+const navButtons = document.getElementById("navButtons");
+
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  var headerTexts = header.getElementsByTagName("a");
+
+  if (window.scrollY > 300) {
+    header.style.backgroundColor = "#3B576F";
+    menuIcon.style.color = "white";
+    closeMenu.style.color = "white";
+    for (var i = 0; i < headerTexts.length; i++) {
+      headerTexts[i].style.color = "white";
+    }
+  } else {
+    header.style.backgroundColor = "#cccccc";
+    menuIcon.style.color = "black";
+    closeMenu.style.color = "black";
+    for (var i = 0; i < headerTexts.length; i++) {
+      headerTexts[i].style.color = "";
+    }
+  }
+});
+
+navButtons.addEventListener("click", function () {
+  navMobile.classList.remove("active");
+  closeMenu.style.display = "none";
+  menuIcon.style.display = "flex";
+});
 
 menuIcon.addEventListener("click", function () {
   navMobile.classList.toggle("active");
